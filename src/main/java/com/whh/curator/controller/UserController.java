fec473d570bb7d0ec7247b8f43014657bcc69086
wyz <1478129502@qq.com>
2023-12-19 01:29:55 +0800

    学生用户管理-基本完成

--- src/main/java/com/whh/curator/controller/UserController.java
+++ src/main/java/com/whh/curator/controller/UserController.java
@@ -6,22 +6,39 @@ import com.whh.curator.domain.po.PageResult;
 import com.whh.curator.domain.po.User;
 import com.whh.curator.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private IUserService userService;
 
-    @GetMapping("list")
+    @GetMapping("/list")
     public PageResult<User> list(Integer pageSize, Integer currentPage) {
         Page<User> page = Page.of(currentPage, pageSize);
         userService.page(page.addOrder(new OrderItem("username", true)));
         return new PageResult<>(page.getTotal(), page.getRecords());
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable String id){
+        return userService.getById(id);
+    }
+
+    @PostMapping
+    public void save(@RequestBody User user){
+        userService.save(user);
+    }
+
+    @PutMapping
+    public void updateById(@RequestBody User user){
+        userService.updateById(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable Integer id){
+        userService.removeById(id);
     }
 }
